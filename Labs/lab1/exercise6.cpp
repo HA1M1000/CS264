4ed6317fb9f6a330af34938659b91dc31fd4c53b
Haim Tagamlitsky <haimt26@hotmail.com>
2018-09-30 19:41:35 +0100

    Update exercise6.cpp

--- Labs/lab1/exercise6.cpp
+++ Labs/lab1/exercise6.cpp
@@ -1,26 +1,22 @@
 //HAIM TAGAMLITSKY - 16306516
 #include <iostream>
 
-int main()
+void sqare(int num)
 {
-  int size;
-  std::cout <<"Please enter a size: ";
-  std::cin >> size;
-
-  for(int i=1; i<=size; i++)
+  for(int i=1; i<=num; i++)
   {
-    if(i == 1 || i==size)
+    if(i == 1 || i==num)
     {
-      for(int j =0; j<size; j++)
+      for(int j =0; j<num; j++)
       {
         std::cout << "+" << " ";
       }
     }
     else
     {
-      for(int j =0; j<size; j++)
+      for(int j =0; j<num; j++)
       {
-        if(j== 0 || j == size-2)
+        if(j== 0 || j == num-2)
         {
           std::cout << "+" << " ";
         }
@@ -30,6 +26,50 @@ int main()
 
     std::cout << " \n";
   }
+}
+/*
+void rectangle(int num)
+{
+  for(int i=1; i<=num; i++)
+  {
+    if(i == 1 || i==num)
+    {
+      for(int j =0; j<num; j++)
+      {
+        std::cout << "+" << " ";
+      }
+    }
+    else if(i > i/2)
+    {
+      for(int j =0; j<num; j++)
+      {
+        if(j== 0 || j == num-2)
+        {
+          std::cout << "+" << " ";
+        }
+        std::cout << " " << " ";
+      }
+    }
+    std::cout << " \n";
+  }
+}
+
+void triangle(int num)
+{
+
+}
+/**/
+int main()
+{
+  int size;
+  std::cout <<"Please enter a size: ";
+  std::cin >> size;
+
+  sqare(size);
+  std::cout << " \n";
+  /*rectangle(size);/**/
+
+
 
   return 0;
 }
